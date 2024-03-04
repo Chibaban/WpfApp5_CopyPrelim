2d11a96a1f9ab2c6aa416fd806bc57d61486158c
22-0033c <22-0033c@CCL1-01A>
2024-03-04 17:26:32 +0800

    Add project files.

---
+++ WpfApp5_CopyPrelim/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp5_CopyPrelim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool _timerStatus = true;
+        DispatcherTimer _dt = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _dt = new DispatcherTimer();
+            _dt.Tick += _dt_Tick;
+            btnFinish.IsEnabled = false;
+        }
+
+        private void _dt_Tick(object sender, EventArgs e)
+        {
+            int sec = int.Parse(lblTimer.Content.ToString());
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnFinish.IsEnabled = true;
+        }
+
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RandomNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
